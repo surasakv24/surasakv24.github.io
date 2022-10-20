f33097487c8b97d30c53933804de4acd7534e53a
surasakv24 <surasakv24@gmail.com>
2022-10-21 01:07:24 +0700

    feat: add getBrandTitle to profile lib

--- libs/profile/index.ts
+++ libs/profile/index.ts
@@ -4,10 +4,16 @@ type Data = {
   [index: string]: any
 }
 
+const BRAND_TITLE_KEY = 'brandTitle'
+
 const hasKey = (key: string): boolean => {
   return key in data
 }
 
 const getDataFromKey = (key: string) => {
   return hasKey(key) ? data[key] : undefined
+}
+
+export const getBrandTitle = (): string => {
+  return getDataFromKey(BRAND_TITLE_KEY) ?? ''
 }
