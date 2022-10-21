390cfd5703aa26ddc1487c106b16a12383e1768b
surasakv24 <surasakv24@gmail.com>
2022-10-21 13:47:24 +0700

    perf: assign type to profile lib

--- libs/profile/index.ts
+++ libs/profile/index.ts
@@ -1,9 +1,11 @@
 import { get } from '@/libs/json'
 import type { JsonObject } from '@/libs/json/types'
+import type { BrandTitle } from './types'
+
 const data: JsonObject = require('@/data.json')
 
 export const BRAND_TITLE_KEY = 'brandTitle'
 
-export const getBrandTitle = (): string => {
+export const getBrandTitle = (): BrandTitle => {
   return get(data, BRAND_TITLE_KEY) ?? ''
 }
