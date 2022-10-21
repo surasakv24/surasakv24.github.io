44af1dc596f0fd7493c2083d835426a63e0febbe
surasakv24 <surasakv24@gmail.com>
2022-10-21 13:42:33 +0700

    feat: add config lib

---
+++ libs/config/index.ts
@@ -0,0 +1,21 @@
+import { get } from '@/libs/json'
+import type { JsonObject } from '@/libs/json/types'
+import type { ImagePath, FilePath, Links } from './types'
+
+const config: JsonObject = require('@/config.json')
+
+export const IMAGE_PATH_KEY = 'imagePath'
+export const FILE_PATH_KEY = 'filePath'
+export const LINKS_KEY = 'links'
+
+export const getImagePath = (): ImagePath => {
+  return get(config, IMAGE_PATH_KEY) ?? '/public/'
+}
+
+export const getFilePath = (): FilePath => {
+  return get(config, FILE_PATH_KEY) ?? '/public/'
+}
+
+export const getLinks = (): Links => {
+  return get(config, LINKS_KEY) ?? []
+}
