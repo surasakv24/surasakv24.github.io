af09e67953127927541f0cd3bc9bc6d73e1fbe02
surasakv24 <surasakv24@gmail.com>
2022-10-24 02:04:47 +0700

    feat: add skills page

--- pages/skills.tsx
+++ pages/skills.tsx
@@ -1,14 +1,64 @@
-import { Layout } from '@/components'
+import {
+  CardBody,
+  CardContainer,
+  CardDark,
+  ImageWithTitle,
+  Layout,
+} from '@/components'
+import { GetStaticProps } from 'next'
 import { ReactNode } from 'react'
-import { NextPageWithLayout } from './_app'
+import { getTechnologySkills } from '@/libs/profile'
+import type { TechnologySkill } from '@/libs/profile/types'
 
-type SkillsProps = {}
+type SkillsProps = {
+  technologies: TechnologySkill[]
+}
 
-const Skills: NextPageWithLayout = (props: SkillsProps) => {
+export const getStaticProps: GetStaticProps<SkillsProps> = () => {
+  return {
+    props: {
+      technologies: getTechnologySkills(),
+    },
+  }
+}
+
+const Skills = ({ technologies }: SkillsProps) => {
   return (
-    <div>
-      <h1>Skills page</h1>
-    </div>
+    <CardDark>
+      <header className="card-header d-flex justify-content-center">
+        <h1>
+          <strong>
+            <ins>Skills</ins>
+          </strong>
+        </h1>
+      </header>
+      <CardBody>
+        <CardContainer>
+          <h2 className="border-top border-bottom text-center">
+            <strong>Technologies skill</strong>
+          </h2>
+          <div className="container text-center mt-3">
+            <div className="row g-3 row-cols-3 row-cols-md-6 justify-content-center">
+              {technologies?.map(
+                ({ image, title }: TechnologySkill, indexLogo) => (
+                  <ImageWithTitle
+                    containerClassName="flex-column"
+                    image={{
+                      src: image,
+                      alt: title,
+                      width: 50,
+                      height: 50,
+                    }}
+                    title={title}
+                    key={`${indexLogo}-logo`}
+                  />
+                )
+              )}
+            </div>
+          </div>
+        </CardContainer>
+      </CardBody>
+    </CardDark>
   )
 }
 
