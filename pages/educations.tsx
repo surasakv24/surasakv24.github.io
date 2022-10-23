6bee72cbee19f9a72fd456c823ba7bc15d78c475
surasakv24 <surasakv24@gmail.com>
2022-10-24 02:20:18 +0700

    feat: add educations page

--- pages/educations.tsx
+++ pages/educations.tsx
@@ -1,14 +1,60 @@
-import { Layout } from '@/components'
+import { CardBody, CardContainer, CardDark, Layout } from '@/components'
+import { GetStaticProps } from 'next'
 import { ReactNode } from 'react'
-import { NextPageWithLayout } from './_app'
+import { getEducations } from '@/libs/profile'
+import type { Education } from '@/libs/profile/types'
 
-type EducationsProps = {}
+type EducationsProps = {
+  educations: Education[]
+}
 
-const Educations: NextPageWithLayout = (props: EducationsProps) => {
+export const getStaticProps: GetStaticProps<EducationsProps> = () => {
+  return {
+    props: {
+      educations: getEducations(),
+    },
+  }
+}
+
+const Educations = ({ educations }: EducationsProps) => {
   return (
-    <div>
-      <h1>Educations page</h1>
-    </div>
+    <>
+      <CardDark>
+        <header className="card-header d-flex justify-content-center">
+          <h1>
+            <strong>
+              <ins>Educations</ins>
+            </strong>
+          </h1>
+        </header>
+      </CardDark>
+      {educations.map(
+        (
+          { degree, faculty, gpa, start, end, institution }: Education,
+          index
+        ) => (
+          <CardDark key={`${index}-education`} className="mt-1">
+            <CardBody>
+              <CardContainer>
+                <div className="text-center">
+                  <h2 className="border-top border-bottom text-center">
+                    <strong>{degree}</strong>
+                  </h2>
+                  <h3>{faculty}</h3>
+                  <div>
+                    <span>GPA. {gpa}</span>
+                  </div>
+                  <span className="text-muted">{`${start} - ${end}`}</span>
+                  <div>
+                    <span>{institution}</span>
+                  </div>
+                </div>
+              </CardContainer>
+            </CardBody>
+          </CardDark>
+        )
+      )}
+    </>
   )
 }
 
