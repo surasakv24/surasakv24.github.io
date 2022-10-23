783adcc27edf6850060c346cc7f86033a9906ff0
surasakv24 <surasakv24@gmail.com>
2022-10-24 02:33:02 +0700

    feat: add contacts page

--- pages/contacts.tsx
+++ pages/contacts.tsx
@@ -1,14 +1,49 @@
-import { Layout } from '@/components'
+import { CardBody, CardDark, ImageWithTitle, Layout } from '@/components'
 import { ReactNode } from 'react'
-import { NextPageWithLayout } from './_app'
+import { getContacts } from '@/libs/profile'
+import type { Contact } from '@/libs/profile/types'
+import { GetStaticProps } from 'next'
 
-type ContactsProps = {}
+type ContactsProps = {
+  contacts: Contact[]
+}
 
-const Contacts: NextPageWithLayout = (props: ContactsProps) => {
+export const getStaticProps: GetStaticProps<ContactsProps> = () => {
+  return {
+    props: {
+      contacts: getContacts(),
+    },
+  }
+}
+
+const Contacts = ({ contacts }: ContactsProps) => {
+  const iconWidth = 45
+  const iconHeight = 45
   return (
-    <div>
-      <h1>Contacts page</h1>
-    </div>
+    <CardDark>
+      <header className="card-header d-flex justify-content-center">
+        <h1>
+          <strong>
+            <ins>Contacts</ins>
+          </strong>
+        </h1>
+      </header>
+      <CardBody className="d-flex flex-column gap-4">
+        {contacts.map(({ title, image, text }: Contact, index) => (
+          <ImageWithTitle
+            containerClassName="flex-column justify-content-center text-center"
+            image={{
+              src: image,
+              alt: title,
+              width: iconWidth,
+              height: iconHeight,
+            }}
+            title={text}
+            key={`${index}-contact`}
+          />
+        ))}
+      </CardBody>
+    </CardDark>
   )
 }
 
