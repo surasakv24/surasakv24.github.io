0253e4a2d92ac95df5b097ba46a0c036fcbb9b5c
surasakv24 <surasakv24@gmail.com>
2022-10-24 01:37:26 +0700

    feat: add experience page

--- pages/experiences.tsx
+++ pages/experiences.tsx
@@ -1,14 +1,101 @@
-import { Layout } from '@/components'
+import {
+  CardBody,
+  CardContainer,
+  CardDark,
+  ImageWithTitle,
+  Layout,
+} from '@/components'
+import { GetStaticProps } from 'next'
 import { ReactNode } from 'react'
-import { NextPageWithLayout } from './_app'
+import { getExperiences } from '@/libs/profile'
+import type { Experience, UseTechnology } from '@/libs/profile/types'
+import Link from 'next/link'
 
-type ExperiencesProps = {}
+type ExperiencesProps = {
+  experiences: Experience[]
+}
 
-const Experiences: NextPageWithLayout = (props: ExperiencesProps) => {
+export const getStaticProps: GetStaticProps<ExperiencesProps> = () => {
+  return {
+    props: {
+      experiences: getExperiences(),
+    },
+  }
+}
+
+const Experiences = ({ experiences }: ExperiencesProps) => {
   return (
-    <div>
-      <h1>Experiences page</h1>
-    </div>
+    <CardDark>
+      <header className="card-header d-flex justify-content-center">
+        <h1>
+          <strong>
+            <ins>Experiences</ins>
+          </strong>
+        </h1>
+      </header>
+
+      <CardBody>
+        <CardContainer>
+          {experiences.map(
+            (
+              {
+                title,
+                start,
+                end,
+                company,
+                location,
+                link,
+                details,
+                useTechnologies,
+              }: Experience,
+              index
+            ) => (
+              <div key={`${index}-experience`}>
+                <div className="text-center">
+                  <h2 className="border-top border-bottom">
+                    <strong className="text-capitalize">{title}</strong>
+                  </h2>
+                  <div className="text-muted">{`${start} - ${end}`}</div>
+                  <div>
+                    <Link href={link}>
+                      <a className="link-light">{company}</a>
+                    </Link>
+                  </div>
+                  <span>{location}</span>
+                </div>
+                <ul className="pt-1">
+                  {details?.map((detail, indexDetail) => (
+                    <li key={`${indexDetail}-detail`}>
+                      <span>{detail}</span>
+                    </li>
+                  ))}
+                </ul>
+                <div className="container text-center">
+                  <h3>Technologies</h3>
+                  <div className="row g-3 row-cols-3 row-cols-md-6 justify-content-center">
+                    {useTechnologies?.map(
+                      ({ image, title }: UseTechnology, indexTech) => (
+                        <ImageWithTitle
+                          containerClassName="flex-column"
+                          image={{
+                            src: `/images/${image}`,
+                            alt: title,
+                            width: 50,
+                            height: 50,
+                          }}
+                          title={title}
+                          key={`${indexTech}-tech`}
+                        />
+                      )
+                    )}
+                  </div>
+                </div>
+              </div>
+            )
+          )}
+        </CardContainer>
+      </CardBody>
+    </CardDark>
   )
 }
 
