e7e6ceed44ce13a8e2d91789f7c84f427647401f
surasakv24 <surasakv24@gmail.com>
2022-10-24 00:04:33 +0700

    feat: add getProfile function to profile lib

--- libs/profile/types.ts
+++ libs/profile/types.ts
@@ -1,2 +1,17 @@
 export type BrandTitle = string
 export type ResumeFile = string
+export type Social = {
+  link: string
+  title: string
+  image: string
+}
+
+export type Profile = {
+  profileImage: string
+  profileImageWidth: number | string
+  profileImageHeight: number | string
+  fullName: string
+  jobTitle: string
+  socials?: Social[]
+  aboutMe?: string[]
+}
