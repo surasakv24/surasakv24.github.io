f0f9bd1ccdbd6a664739a515614450452ebf081b
surasakv24 <surasakv24@gmail.com>
2022-10-26 00:06:08 +0700

    fix: #1 placeholder on loading profile image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,39 @@
-import { CardBody, CardDark, ImageLink, Layout } from '@/components'
-import { GetStaticProps } from 'next'
+import {
+  CardBody,
+  CardDark,
+  Figure,
+  FigureCaption,
+  ImageLink,
+  Layout,
+} from '@/components'
+import { InferGetStaticPropsType } from 'next'
 import { ReactNode } from 'react'
 import { getProfile } from '@/libs/profile'
-import type { Profile, Social } from '@/libs/profile/types'
+import type { Social } from '@/libs/profile/types'
 import Image from 'next/image'
+import { getPlaiceholder } from 'plaiceholder'
 
-type HomeProps = {
-  profile: Profile
-}
+export const getStaticProps = async () => {
+  const profile = getProfile()
+  const { profileImage } = profile
+  const { base64, img } = await getPlaiceholder(profileImage)
 
-export const getStaticProps: GetStaticProps<HomeProps> = () => {
   return {
     props: {
-      profile: getProfile(),
+      image: {
+        ...img,
+        blurDataURL: base64,
+      },
+      profile,
     },
   }
 }
 
-const Home = ({ profile }: HomeProps) => {
+const Home = ({
+  profile,
+  image,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   const {
-    profileImage,
     profileImageWidth,
     profileImageHeight,
     fullName,
@@ -29,26 +43,26 @@ const Home = ({ profile }: HomeProps) => {
   } = profile
   return (
     <CardDark className="pt-2">
-      <div className="mx-auto p-3">
-        <Image
-          src={profileImage}
-          alt="avatar"
-          className="rounded-circle border border-3"
-          width={profileImageWidth}
-          height={profileImageHeight}
-          objectFit="cover"
-          objectPosition="50% 25%"
-        />
-      </div>
-
-      <CardBody>
-        <div className="text-center text-capitalize">
-          <h1>
+      <Figure className="px-3 pt-3">
+        <div className="d-flex justify-content-center">
+          <Image
+            {...image}
+            placeholder="blur"
+            className="rounded-circle border border-3"
+            width={profileImageWidth}
+            height={profileImageHeight}
+            objectFit="cover"
+            objectPosition="50% 25%"
+            alt={fullName}
+          />
+        </div>
+        <FigureCaption className="text-center text-capitalize mt-3">
+          <h1 className="text-light">
             <strong>{fullName}</strong>
           </h1>
           <h2 className="border-top border-bottom text-muted">{jobTitle}</h2>
-        </div>
-      </CardBody>
+        </FigureCaption>
+      </Figure>
 
       <CardBody>
         <aside className="d-flex justify-content-center gap-3">
